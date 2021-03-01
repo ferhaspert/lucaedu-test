97a94d52504b6233c18ce0dcf61dc49c62c16bbd
Fernando Haspert <fhaspert@technisys.com>
2021-03-01 00:00:56 -0300

    card styles

--- components/comunity/QuestionsCard.jsx
+++ components/comunity/QuestionsCard.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, CardTitle, CardText, CardFooter, Row, Col } from "reactstrap";
 import { ReactSVG } from "react-svg";
+import useCheckMobileScreen from "../../utils/pageWidth";
 
 export default function QuestionCard({
   id,
@@ -20,50 +21,74 @@ export default function QuestionCard({
     return array.some((id) => id === user?.id);
   };
 
+  const [isMobile, _] = useState(useCheckMobileScreen());
+
+  const renderLikes = () => (
+    <div className="like">
+      <div className={`${userMatch(likedBy) ? "liked" : ""}`}>
+        <ReactSVG
+          onClick={() => toggleReaction(id, "like")}
+          src="/icons/thumbs-up.svg"
+        />
+      </div>
+      <div className={`${userMatch(dislikedBy) ? "disliked" : ""} `}>
+        <ReactSVG
+          onClick={() => toggleReaction(id, "dislike")}
+          src="/icons/thumbs-down.svg"
+        />
+      </div>
+    </div>
+  );
+
   return (
     <Row key={id}>
       <Card body>
-        <Col className="avatar" sm="2">
+        <Col className="avatar" xs={isMobile ? 3 : 2}>
           <img src={`/icons/avatar-${avatar}.svg`} />
+          {isMobile && renderLikes()}
         </Col>
-        <Col sm="8">
+        <Col xs={isMobile ? 9 : 8}>
           <Row>
             <CardTitle>{title}</CardTitle>
             <CardText>{description}</CardText>
             <CardFooter>
-              <div className="like">
-                <div className={`${userMatch(likedBy) ? "liked" : ""}`}>
-                  <ReactSVG
-                    onClick={() => toggleReaction(id, "like")}
-                    src="/icons/thumbs-up.svg"
-                  />
-                </div>
-                <div className={`${userMatch(dislikedBy) ? "disliked" : ""} `}>
-                  <ReactSVG
-                    onClick={() => toggleReaction(id, "dislike")}
-                    src="/icons/thumbs-down.svg"
-                  />
-                </div>
-              </div>
+              {!isMobile && renderLikes()}
               <div className="student">
-                Pregunta {username} en <span className="course">{course}</span>
+                {`${isMobile ? "" : "Pregunta"} ${username} en`}{" "}
+                <span className="course">{course}</span>
               </div>
+              {isMobile && (
+                <div className="actions">
+                  <div className="comments">
+                    <ReactSVG src="/icons/message-circle.svg" />
+                    <p>{comments}</p>
+                  </div>
+                  <div className="share-fav">
+                    <ReactSVG
+                      onClick={() => toggleReaction(id, "fav")}
+                      src={`/icons/${favourite ? "filled-" : ""}star.svg`}
+                    />
+                  </div>
+                </div>
+              )}
             </CardFooter>
           </Row>
         </Col>
-        <Col className="actions" sm="2">
-          <div className="comments">
-            <ReactSVG src="/icons/message-circle.svg" />
-            <p>{comments}</p>
-          </div>
-          <div className="share-fav">
-            <ReactSVG src="/icons/share.svg" />
-            <ReactSVG
-              onClick={() => toggleReaction(id, "fav")}
-              src={`/icons/${favourite ? "filled-" : ""}star.svg`}
-            />
-          </div>
-        </Col>
+        {!isMobile && (
+          <Col className="actions" xs="2">
+            <div className="comments">
+              <ReactSVG src="/icons/message-circle.svg" />
+              <p>{comments}</p>
+            </div>
+            <div className="share-fav">
+              <ReactSVG src="/icons/share.svg" />
+              <ReactSVG
+                onClick={() => toggleReaction(id, "fav")}
+                src={`/icons/${favourite ? "filled-" : ""}star.svg`}
+              />
+            </div>
+          </Col>
+        )}
       </Card>
     </Row>
   );
